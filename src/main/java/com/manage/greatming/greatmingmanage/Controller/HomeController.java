7fa77e367da78f6a2c0fea4ed45bade3e78f3a8c
yibajianghudao <w2027075535@outlook.com>
2024-09-22 23:42:48 +0800

    release 1.0

--- src/main/java/com/manage/greatming/greatmingmanage/Controller/HomeController.java
+++ src/main/java/com/manage/greatming/greatmingmanage/Controller/HomeController.java
@@ -1,65 +1,16 @@
 package com.manage.greatming.greatmingmanage.Controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.manage.greatming.greatmingmanage.DAO.User;
-import com.manage.greatming.greatmingmanage.Service.UserService;
-
-
-
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/")
 public class HomeController {
-
-    @Autowired
-    private UserService userService;
     
     @GetMapping
-    public String ListUsers(Model model) {
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
-        return "user/list";
+    public String Home(){
+        return "/home.html";
     }
 
-    @GetMapping("/create")
-    public String showCreateForm(Model model) {
-        model.addAttribute("user", new User());
-        return "user/create";
-    }
-
-    @PostMapping
-    public String createUser(@ModelAttribute User user) {
-        userService.addNewUser(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String showEditForm(@PathVariable Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "user/edit";
-    }
-
-    @PostMapping("/update/{id}")
-    public String updateUser(@PathVariable Long id, @ModelAttribute User user) {
-        user.setId(id);
-        userService.updateUser(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteUser(@PathVariable Long id) {
-        userService.delete(id);
-        return "redirect:/users";
-    }
-    
 }
